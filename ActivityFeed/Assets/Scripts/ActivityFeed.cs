059be039e95977f0e3b2ee6f6b7489f45e955ea7
Alex <S00190272@mail.itsligo.ie>
2018-11-06 10:54:45 +0000

    activityfeed

---
+++ ActivityFeed/Assets/Scripts/ActivityFeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivityFeed : MonoBehaviour {
+
+    public GameObject KillFeed;
+    public GameObject ContentPanel;
+    public Image image;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AddMessage("Test", "Test", image, Color.red, 5f);
+        }
+    }
+
+    void AddMessage(string leftMessage, string rightMessage, Image image, Color color, float lifeTime)
+    {
+        var text = Instantiate(KillFeed, ContentPanel.transform);
+
+        var activityText = text.GetComponent<ActivityText>();
+        activityText.InitText(leftMessage, rightMessage, image, color, lifeTime);
+    }
+
+}
--- ActivityFeed/Assets/Scripts/ActivityText.cs
+++ ActivityFeed/Assets/Scripts/ActivityText.cs
@@ -8,6 +8,7 @@ public class ActivityText : MonoBehaviour {
     // left message [Sprite] right message
     public Text leftText;
     public Text rightText;
+    public Image image;
 
 	// Use this for initialization
 	void RemoveText () {
@@ -15,12 +16,13 @@ public class ActivityText : MonoBehaviour {
 	}
 
     // Update is called once per frame
-    void InitText(string leftMessage, string rightMessage, float lifeTime, Color color)
+    public void InitText(string leftMessage, string rightMessage, Image uImage,  Color color, float lifeTime)
     {
         leftText.text = leftMessage;
         leftText.color = color;
         rightText.text = rightMessage;
         rightText.color = color;
+        image = uImage;
 
         Invoke("RemoveText", lifeTime);
 
