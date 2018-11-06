9505ae92f76eee200e902ecce786d0e84e31beb3
Alex <S00190272@mail.itsligo.ie>
2018-11-06 10:02:39 +0000

    unity

---
+++ ActivityFeed/Assets/Scripts/ActivityText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivityText : MonoBehaviour {
+
+    // left message [Sprite] right message
+    public Text leftText;
+    public Text rightText;
+
+	// Use this for initialization
+	void RemoveText () {
+        Destroy(gameObject);
+	}
+
+    // Update is called once per frame
+    void InitText(string leftMessage, string rightMessage, float lifeTime, Color color)
+    {
+        leftText.text = leftMessage;
+        leftText.color = color;
+        rightText.text = rightMessage;
+        rightText.color = color;
+
+        Invoke("RemoveText", lifeTime);
+
+    }
+}
