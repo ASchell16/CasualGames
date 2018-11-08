a71450e36318e6205f16a47e54d83cab33ec1833
Alex <S00190272@mail.itsligo.ie>
2018-11-08 15:35:46 +0000

    UnityStarterKit/GameDesignandunity

---
+++ Unity Starter Kit/Starter Kit/Rays/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+
+    // Use this for initialization
+    Rigidbody body;
+    public float Jump = 1;
+    public float Rotation = 1f;
+    public float Move = .5f;
+    float horInput;
+    float vertInput;
+    bool canJump;
+    Vector3 force = Vector3.zero;
+
+	void Start ()
+    {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        horInput = Input.GetAxis("Horizontal") * Move;
+        vertInput = Input.GetAxis("Vertical") * Move;
+        body.AddForce(horInput, 0, vertInput);
+        if (Input.GetKeyUp(KeyCode.Space) && canJump)
+        {
+            body.AddForce(0, Jump, 0, ForceMode.Impulse);
+            canJump = false;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, Rotation, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, -Rotation, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, Move);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0,-Move);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Move, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Move, 0, 0);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+
+}
---
+++ Unity Starter Kit/Starter Kit/Rays/Assets/Scripts/Tracker.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Tracker : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform Track;
+    public bool DrawLine;
+    public int Distance = 100;
+    LineRenderer lineRenderer;
+    RaycastHit result;
+    int trackingDistance = 10;
+    public bool usetrackingDistance = false;
+
+    void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Track);
+
+        if (usetrackingDistance && Vector3.Distance(transform.position, Track.position) <= trackingDistance) {
+            if (DrawLine)
+            {
+                lineRenderer.SetPosition(0, transform.position);
+                if (Physics.Raycast(transform.position, transform.forward, out result, Distance))
+                {
+                    if (result.collider.tag != "Player")
+                    {
+                        lineRenderer.SetPosition(1, result.point);
+                    }
+                    else
+                    {
+                        lineRenderer.SetPosition(1, Track.position);
+                    }
+                }
+
+
+            }
+        }
+
+	}
+
+    private void OnDrawGizmos()
+    {
+        if (gameObject.tag != "MainCamera")
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, trackingDistance);
+        }
+    }
+}
